25670307ea02503c610fbbe3527211f57a158fad
mgabrielps <36735225+mgabrielps@users.noreply.github.com>
2018-06-12 09:41:52 -0400

    Add files via upload

---
+++ src/main/java/JenkinsTest/JenkinstTest/App.java
@@ -0,0 +1,13 @@
+package JenkinsTest.JenkinstTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
